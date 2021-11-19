f823c483a3a8ef57f5eedd83cb9436ba14e7b239
Addison Chan <addison95132@gmail.com>
2021-11-19 11:45:21 -0800

    Add discussion 12

---
+++ 12/12.py
@@ -0,0 +1,24 @@
+import re
+def email_validator(email, domains):
+    """
+    >>> email_validator("oski@berkeley.edu", ["berkeley.edu", "gmail.com"])
+    True
+    >>> email_validator("oski@gmail.com", ["berkeley.edu", "gmail.com"])
+    True
+    >>> email_validator("oski@berkeley.com", ["berkeley.edu", "gmail.com"])
+    False
+    >>> email_validator("oski@berkeley.edu", ["yahoo.com"])
+    False
+    >>> email_validator("xX123_iii_OSKI_iii_123Xx@berkeley.edu", ["berkeley.edu", "gmail.com"])
+    True
+    >>> email_validator("oski@oski@berkeley.edu", ["berkeley.edu", "gmail.com"])
+    False
+    >>> email_validator("oski@berkeleysedu", ["berkeley.edu", "gmail.com"])
+    False
+    """
+    pattern = r"^\w+@("
+    domain_regex = "|".join(domain.replace(".", "\\.") for domain in domains)
+    pattern += domain_regex + ")"
+    return bool(re.search(pattern, email))
+
+    # pattern = r"^(\w+)@(\w+\.\w{3})"
